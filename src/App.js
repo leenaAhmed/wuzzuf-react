984c2c08b19f4476cfc9515d03a6829bfa508c52
Ahmed Alwakeel <alwakeel1996@gmail.com>
2022-03-10 22:22:12 +0200

    intial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+
+ 
+  return (
+   <>
+    
+      <h1>Welcome</h1>
+      
+   </>
+     
+    
+  );
+}
+
+export default App;
