baae5a7f80dac9c7bd3871e414a35724c7f8c2ad
leenaAhmed <leena3792@gmail.com>
2022-03-16 16:26:05 +0200

    add language

--- src/components/jobModule/applayToJob/index.js
+++ src/components/jobModule/applayToJob/index.js
@@ -1,14 +1,21 @@
 import "./style.scss";
 import { useParams, useHistory } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import download from "./../../../assets/download.png";
 import explorjob from "../../../services/explorjob";
 import appliction from "../../../services/appliction";
+import ar from "../../../language/explore/ar.json";
+import en from "../../../language/explore/en.json";
+import { languageContext } from "../../../contexts/languageContext";
+
 export default function ApplyToJob(props) {
   let { jobId } = useParams();
   let { companyId } = useParams();
   let history = useHistory();
+  const { lang, setLang } = useContext(languageContext);
+  const [json, setJson] = useState(en);
   const [jobDetails, setjobrDetails] = useState({});
+
   const [applicion, setApplicion] = useState({
     currentjob: "",
     position: "",
@@ -46,47 +53,64 @@ export default function ApplyToJob(props) {
     });
   };
   useEffect(() => {
+    if (lang === "English") {
+      setJson(en);
+    }
+    if (lang === "العربية") {
+      setJson(ar);
+    }
+  }, [lang]);
+  useEffect(() => {
     loadingdetails();
     console.log(jobDetails);
   }, []);
   return (
     <>
-      <div className="container">
+      <div className="container" dir={lang === "English" ? "ltr" : "rtl"}>
         <div className="row mt-5 m-0 p-0">
           <div className="col-lg-3 col-md-6 col-sm-12 rounded">
             <div className=" rounded p-2 m-5 smallanswerdiv">
-              <p className="font13">
-                Your answers reflect your vital skills and experience. Providing
-                excellent answers sets you apart from the competition.
-              </p>
+              <p className="font13">{json.answer}</p>
             </div>
           </div>
           <div className="card col-lg-6 col-md-12 col-sm-12 mb-5">
-            <div className="rounded d-flex  justify-content-between text-light fs-5 p-2 m-0   Appform">
-              Application Form
+            <div className="rounded d-flex  justify-content-between text-light fs-5 p-2 m-0 Appform">
+              {json.form}
               <button
                 className="btn btn-light btn-sm "
                 onClick={() => backHandelar()}
                 type="button"
               >
-                View job details
+                {json.View}
               </button>
             </div>
-            <div className="p-3 m-0 ">
+            <div className="p-3 m-0">
               <img
-                className="float-end mt-4"
+                className={`${
+                  lang === "English" ? "float-end" : "float-start"
+                } mt-4 `}
                 src={`${!jobDetails.logo ? download : jobDetails.logo}`}
                 width="85px"
                 alt={props.title}
               />
 
-              <h5 className="text-dark fw-bold job_title">
-                {jobDetails.jobTitle}{" "}
+              <h5 className="text-dark fw-bold job_title ">
+                {lang === "English" ? (
+                  <span> {jobDetails.jobTitle}</span>
+                ) : (
+                  <span>{jobDetails.jobTitleAR}</span>
+                )}
               </h5>
               <span className="font13 hiringteam">
-                The hiring team at
-                <strong className="job_title">{jobDetails.companyName}</strong>
-                requires you to answer the below questions.
+                {json.applyToJob}
+                <strong className="job_title  ">
+                  {lang === "English" ? (
+                    <span>{jobDetails.companyName}</span>
+                  ) : (
+                    <span>{jobDetails.companyName}</span>
+                  )}
+                </strong>
+                {json.applyToRequire}
               </span>
               <form onSubmit={(e) => submitHandelar(e)}>
                 <div className="mb-3 mt-4">
@@ -94,14 +118,14 @@ export default function ApplyToJob(props) {
                     htmlFor="formGroupExampleInput"
                     className="form-label fw-bold font13"
                   >
-                    Why should we hire you for this position?
+                    {json.position}
                   </label>
                   <div>
                     <textarea
                       className="form-control h70"
                       id="floatingInputValue"
                       name="position"
-                      placeholder="Write your answer here.."
+                      placeholder={json.placeholder}
                       required
                       value={applicion.position}
                       onChange={(e) => changehandlar(e)}
@@ -113,13 +137,13 @@ export default function ApplyToJob(props) {
                     for="formGroupExampleInput2"
                     className="form-label fw-bold font13"
                   >
-                    Can you provide us with a sample of your work?
+                    {json.sample}
                   </label>
                   <div className="">
                     <textarea
                       className="form-control h70"
                       id="floatingInputValue"
-                      placeholder=" Write your answer here..  "
+                      placeholder={json.placeholder}
                       name="samplework"
                       required
                       value={applicion.samplework}
@@ -132,14 +156,14 @@ export default function ApplyToJob(props) {
                     for="formGroupExampleInput2"
                     className="form-label fw-bold font13"
                   >
-                    Describe your current job responsibilities
+                    {json.responsibilities}
                   </label>
                   <div className="">
                     <textarea
                       className="form-control h70"
                       id="floatingInputValue"
                       name="currentjob"
-                      placeholder=" Write your answer here.. "
+                      placeholder={json.placeholder}
                       required
                       value={applicion.currentjob}
                       onChange={(e) => changehandlar(e)}
@@ -151,20 +175,15 @@ export default function ApplyToJob(props) {
                     type="submit"
                     class="btn btn-primary btn-sm float-end mt-3 m-1 font13"
                   >
-                    Submit Application
+                    {json.submit}
                   </button>
-                  <a
-                    href
-                    className="btn btn-secondary btn-sm float-end mt-3 m-1 font13"
-                  >
-                    Save and Apply later
-                  </a>
+
                   <a
                     href
                     onClick={() => backHandelar()}
                     className="btn btn-secondary btn-sm float-start mt-3 m-1 ms-2 font13"
                   >
-                    Cancel
+                    {json.cancel}
                   </a>
                 </div>
               </form>
